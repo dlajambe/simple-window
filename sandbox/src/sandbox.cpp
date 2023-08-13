d36c0b3bc4faeb0ce4535c8634b74fd56d4c0c8e
David LaJambe <44506412+dlajambe@users.noreply.github.com>
2023-08-13 15:45:20 -0400

    Initialize and configure simple-window and sandbox example project (#1)
    * Initial project setup
    
    * Add header file
    
    * Change to static library
    
    * Fix stupid errors
    
    * Delete file permanently
    
    * Finalize configuration of static library project4
    
    * Add error handling to main

---
+++ sandbox/src/sandbox.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "simple_window.h"
+
+void draw() {
+	std::cout << "Welcome to Sandbox!" << std::endl;
+	simple_window::print();
+}
+
+int main()
+{
+	try {
+		draw();
+	}
+	catch (std::exception ex) {
+		std::cerr << "Exception encountered: " << ex.what() << std::endl;
+	}
+	catch (...) {
+		std::cerr << "Unhandled exception" << std::endl;
+	}
+	
+}
