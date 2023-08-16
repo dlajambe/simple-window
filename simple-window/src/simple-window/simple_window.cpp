f7ed092dddc9850f21dc524a83544cc10d5b3572
David LaJambe <dlajambe@gmail.com>
2023-08-15 22:07:21 -0400

    Get the triangle rendering

--- simple-window/src/simple-window/simple_window.cpp
+++ simple-window/src/simple-window/simple_window.cpp
@@ -20,13 +20,14 @@ namespace simple_window
 	{
 		glfwSetErrorCallback(error_callback);
 
-		// Checks if the GLFW library was successfully initialized
+		// Step 1 - Initialize the GLFW library
 		if (!glfwInit())
 			exit(EXIT_FAILURE);
 
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
+		// Step 2 - Create a window and set callbacks
 		GLFWwindow* window = glfwCreateWindow(640, 480, "Simple Window", NULL, NULL);
 
 		if (!window)
@@ -35,20 +36,65 @@ namespace simple_window
 			exit(EXIT_FAILURE);
 		}
 
+		// Step 3 - Initialize the context
 		glfwSetKeyCallback(window, key_callback);
 		glfwMakeContextCurrent(window);
-		
+
+		std::cout << "GL Connection: " << glGetString(GL_VERSION) << std::endl;
+
+		// Step 4 - With the context initialized, GLEW can be loaded. GLEW loads
+		// all of the modern OpenGL function implementations from the GPU drivers
+		// so that they can be used even though Windows stopped supporting Open GL
+		// decades ago
 		if (glewInit() != GLEW_OK)
 		{
 			throw std::exception("GLEW initialization with glewInit() failed");
 			exit(EXIT_FAILURE);
 		}
 
+		// Step 5 - Create a buffer
+		// To render anything, at least one buffer is required. A buffer ID is required
+		unsigned int buffer_id;
+		glGenBuffers(1, &buffer_id);
+
+		// Binding selects the buffer to which data are to be added
+		glBindBuffer(GL_ARRAY_BUFFER, buffer_id);
+
+		// Sample data to draw a triangle
+		const std::size_t data_size = 6;
+		float positions[data_size] = {
+			-0.5f, 0.5f,
+			0.0f, -0.5f,
+			0.5f, 0.5f
+		};
+
+		// Step 6 - Pass the data to the buffer
+		// Remember - OpenGL is a highly stateful API; the glBufferData function adds the data
+		// to whichever buffer is currently bound
+		glBufferData(GL_ARRAY_BUFFER, data_size * sizeof(float), positions, GL_STATIC_DRAW);
+
+		// Step 7 - Tell OpenGL how the buffer's data are structured so that it can interpret them
+		// corectly while rendering. Once again, due to the stateful nature of OpenGL's API, this
+		// is applied automatically to whatever buffer is currently bound
+
+		// The first (and only) attribute (index 0) is the vertex position.
+		// Each position is 2 floats, so the count is 2 and the slider is 2 * 8 bytes = 16 bytes
+		// There is no offset, so the final argument is set to 0
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+		// The attribute is only used if it is enabled with the following function
+		glEnableVertexAttribArray(0);
+
+		// This controls how often the buffer being displayed is swapped out for the one being rendered
+		// A value of 1 indicates the buffers should be swapped every screen update
 		glfwSwapInterval(1);
 
 		while (!glfwWindowShouldClose(window))
 		{
+			glDrawArrays(GL_TRIANGLES, 0, 3);
+
 			glfwSwapBuffers(window);
+
 			glfwPollEvents();
 		}
 
